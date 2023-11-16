d5b156f19aa28560977deaf63a065c5a56835ede
Angga Sya'ban Rahid <50766782+anggasybnrhd@users.noreply.github.com>
2023-11-16 20:58:18 +0700

    soal no 6 uts smester1 , algoritma pemrograman

---
+++ Variabel .java
@@ -0,0 +1,13 @@
+public class Variabel {
+    public static void main(String[] args) {
+        // input nama pake string
+        String nama = "Angga Sya'ban Rahid";
+        // input umur pake integer
+        int umur = 21;
+
+        // outputnya nama dan umur
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Umur : " + umur + " tahun");
+    }
+}
+// selesai
